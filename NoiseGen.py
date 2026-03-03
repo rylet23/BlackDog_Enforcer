e0263a48aa4741e7a2a8318980719725f79c1951
chaneyerau <rmm02chaney@gmail.com>
2026-03-03 15:08:45 -0500

    copied BasicFreqGen code for NoiseGen to NosieGen on Roberts Branch

--- NoiseGen.py
+++ NoiseGen.py
@@ -3,10 +3,24 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 
-def NoiseOn(target == True):
-    while True:
-        GPIO.output(17, True)
 
-def NoiseOff(target == False):
+def trigger_noise():
     while True:
-	GPIO.output(17, False)
+        GPIO.output(17,True)
+
+def stop_noise():
+    while True:
+        GPIO.output(17,False)
+
+def main():
+
+    target = True
+
+    while True:
+        if target == True:
+            trigger_noise()
+        else:
+            stop_noise()
+        time.sleep(10)
+        target != target
+

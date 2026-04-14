259c80fb010dcbd23312ef1a36a53c1fb5ac4d24
chaneyerau <rmm02chaney@gmail.com>
2026-04-14 15:12:19 -0400

    update NoiseGEn.py

--- NoiseGen/NoiseGen.py
+++ NoiseGen/NoiseGen.py
@@ -3,10 +3,12 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(6, GPIO.OUT)
 
-def NoiseOn(target == True):
+target = False
+
+def NoiseOn(target):
     while True:
         GPIO.output(6, True)
 
-def NoiseOff(target == False):
+def NoiseOff(target):
     while True:
-	GPIO.output(6, False)
+	    GPIO.output(6, False)

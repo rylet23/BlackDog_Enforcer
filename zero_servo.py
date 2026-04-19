9f07991cc98128e7aacb7791617053aaa352ace9
rylet23 <ryletraub@gmail.com>
2026-04-19 00:22:37 -0400

    Chanhges

--- zero_servo.py
+++ zero_servo.py
@@ -9,20 +9,30 @@ GPIO.setup(STEER_PIN, GPIO.OUT)
 
 # --- PWM setup (50Hz) ---
 steer = GPIO.PWM(STEER_PIN, 50)
-steer.start(0)
+steer.start(7.5)
+time.sleep(0.5)
 
 def set_steering(angle):
     """Set steering angle (-100 to 100)"""
-    angle = max(-100, min(100, angle))
+    #angle = max(-100, min(100, angle))
     duty = 7.5 + (angle / 200) * 5
     steer.ChangeDutyCycle(duty)
 
 try:
-    print("Centering steering servo to dead center (0°)...")
-    set_steering(0)
-    time.sleep(2)
+    target = 0
+    set_steering(target)
+    print("holding fr 3 sec")
+    for i in range(5, 0, -1):
+        print(f"{i}..")
+        time.sleep(1)
+#    test_angle = 50
+ #   print(f"Moving to {test_angle}...")
+    #set_steering(test_angle)
+    time.sleep(1)
     
 finally:
+    #set_steering(0)
+    #time.sleep(0.5)
     print("Cleaning up GPIO...")
     steer.stop()
     GPIO.cleanup()
